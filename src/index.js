f5b81f33141353d4a0310a7e72ae4fab033746c8
sakettt25 <saketsauravcse@gmail.com>
2024-09-29 14:30:53 +0530

    modification

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';  // Ensure this imports Tailwind
+import './index.css';
 import App from './App';
 
 ReactDOM.render(
